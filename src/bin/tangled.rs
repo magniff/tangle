b6d026813bc8df114604c53ed40bc07c1f4e1884
magniff <tinysnippets@gmail.com>
2023-02-15 19:22:02 -0800

    Typos.

--- src/bin/tangled.rs
+++ src/bin/tangled.rs
@@ -4,17 +4,17 @@ use tokio;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let switch_arguments = tangle::settings::TangleArguments::parse();
+    let arguments = tangle::settings::TangleArguments::parse();
 
     simplelog::TermLogger::init(
-        switch_arguments.log_level,
+        arguments.log_level,
         simplelog::Config::default(),
         simplelog::TerminalMode::Mixed,
         simplelog::ColorChoice::Auto,
     )?;
 
     tangle::server::run(
-        tokio::net::TcpListener::bind(switch_arguments.server_address).await?,
+        tokio::net::TcpListener::bind(arguments.server_address).await?,
         tokio::signal::ctrl_c(),
     )
     .await
